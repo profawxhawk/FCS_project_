8e31c828c4459c40a0e7bfa8e039bc8d2665688a
profawxhawk <bharath17035@iiitd.ac.in>
2019-10-15 14:13:30 +0530

    added options

--- fcs_project/fcs_project/urls.py
+++ fcs_project/fcs_project/urls.py
@@ -20,6 +20,8 @@ from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
+    path('options/',user_views.options,name='useroptions'),
+    path('welcome/',user_views.welcome,name='welcomepage'),
     path('home',user_views.home,name='homepage'),
     path('',user_views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
--- fcs_project/users/views.py
+++ fcs_project/users/views.py
@@ -3,14 +3,21 @@ from django.contrib.auth import login, authenticate,logout
 from django.urls import reverse
 from .forms import SignUpForm
 from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('/') 
+@login_required
+def options(request):
+    return render(request, 'users/options.html')
 
 def home(request):
     return render(request, 'users/welcome.html')
 
+def welcome(request):
+    return render(request, 'users/home.html')
 
 def signup(request):
     if request.method == 'POST':
