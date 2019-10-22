22a48771c3d6b4f8a9979ddd9aeef7606d2a2227
profawxhawk <bharath17035@iiitd.ac.in>
2019-10-22 21:52:17 +0530

    small changes

--- fcs_project/chat/views.py
+++ fcs_project/chat/views.py
@@ -1,19 +1,26 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.utils.safestring import mark_safe
+from users.models import User
+from django.urls import reverse
 import json
-
+from django_otp.decorators import otp_required
+from friendship.models import Friend, Follow, Block,FriendshipRequest
 def index(request):
     return render(request, 'chat/index.html', {})
 
-@login_required
+@otp_required
 def room(request,pk):
-    print(int(pk))
-    a = min(request.user.id,int(pk))
-    b = max(int(pk),request.user.id)
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(str(a)+str(b))),
-        'username': mark_safe(json.dumps(request.user.username)),
-        'userid': mark_safe(json.dumps(request.user.id)),
-        'toid':mark_safe(json.dumps(pk))
-    })
+    other_user = User.objects.get(pk=pk)
+    if Friend.objects.are_friends(request.user, other_user)==True:
+        a = min(request.user.id,int(pk))
+        b = max(int(pk),request.user.id)
+        return render(request, 'chat/room.html', {
+            'room_name_json': mark_safe(json.dumps(str(a)+str(b))),
+            'username': mark_safe(json.dumps(request.user.username)),
+            'username1': mark_safe(json.dumps(other_user.username)),
+            'userid': mark_safe(json.dumps(request.user.id)),
+            'toid':mark_safe(json.dumps(pk))
+        })
+    else:
+        return redirect(reverse('homepage'))
